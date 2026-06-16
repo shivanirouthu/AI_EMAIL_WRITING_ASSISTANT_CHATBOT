9b26ea260fd35cf4b979a379c648c5b01669fdfe
Shivani <yourgithubemail@gmail.com>
2026-06-16 17:43:07 +0530

    fixed langfuse initialization

--- src/prompt.py
+++ src/prompt.py
@@ -1,10 +1,15 @@
+import os
 from dotenv import load_dotenv
 from langfuse import Langfuse
 from langchain_core.prompts import ChatPromptTemplate
 
 load_dotenv()
 
-langfuse = Langfuse()
+langfuse = Langfuse(
+    public_key=os.getenv("LANGFUSE_PUBLIC_KEY"),
+    secret_key=os.getenv("LANGFUSE_SECRET_KEY"),
+    host=os.getenv("LANGFUSE_HOST")
+)
 
 print("AUTH CHECK:")
 print(langfuse.auth_check())
